472a35585081e803a2174c93bfc9d684add92c31
BAILLEUL <bailleul.sacha@gmail.com>
2021-06-07 17:44:39 +0200

    [feat]: Run typescript

---
+++ CodingChainApi.Application/Contracts/Dtos/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.ProgrammingLanguages;
+
+namespace Application.Contracts.Dtos
+{
+    public record RunParticipationTestsDto(Guid Id, LanguageEnum Language, string? HeaderCode,
+        IList<RunParticipationTestsDto.Test> Tests,
+        IList<RunParticipationTestsDto.Function> Functions)
+    {
+        public record Test(Guid Id, string Name, string OutputValidator, string InputGenerator);
+
+        public record Function(Guid Id, string Code, int Order);
+    }
+    
+    public record Function(Guid Id, string Code);
+
+    public record PlagiarismAnalyzeExecutionDto
+        (Function SuspectedFunction, IList<Function> ComparedFunctions);
+    
+    public record PrepareParticipationExecutionDto(Guid Id, LanguageEnum Language);
+    public record CleanParticipationExecutionDto(Guid Id, LanguageEnum Language);
+
+}
---
+++ CodingChainApi.Infrastructure/Services/Messaging/Dispatchers.cs
@@ -0,0 +1,48 @@
+﻿using Application.Contracts.Dtos;
+using CodingChainApi.Infrastructure.Services.Processes;
+using CodingChainApi.Infrastructure.Settings;
+using Microsoft.Extensions.Logging;
+
+namespace CodingChainApi.Infrastructure.Services.Messaging
+{
+    public class ParticipationPendingExecutionService : BaseDispatcherService<RunParticipationTestsDto>
+    {
+        public ParticipationPendingExecutionService(IRabbitMqSettings settings,
+            ILogger<ParticipationPendingExecutionService> logger) : base(settings, logger)
+        {
+            Exchange = settings.ParticipationExchange;
+            RoutingKey = settings.PendingExecutionRoutingKey;
+        }
+    }
+
+    public class PlagiarismPendingExecutionService : BaseDispatcherService<PlagiarismAnalyzeExecutionDto>
+    {
+        public PlagiarismPendingExecutionService(IRabbitMqSettings settings,
+            ILogger<PlagiarismPendingExecutionService> logger) : base(settings, logger)
+        {
+            Exchange = settings.PlagiarismExchange;
+            RoutingKey = settings.PlagiarismAnalyzeExecutionRoutingKey;
+        }
+    }
+
+    public class PrepareParticipationExecutionService : BaseDispatcherService<PrepareParticipationExecutionDto>
+    {
+        public PrepareParticipationExecutionService(IRabbitMqSettings settings,
+            ILogger<PrepareParticipationExecutionService> logger) : base(settings, logger)
+        {
+            Exchange = settings.ParticipationExchange;
+            RoutingKey = settings.PrepareExecutionRoutingKey;
+        }
+    }
+    
+    public class CleanParticipationExecutionService : BaseDispatcherService<CleanParticipationExecutionDto>
+    {
+        public CleanParticipationExecutionService(IRabbitMqSettings settings,
+            ILogger<CleanParticipationExecutionService> logger) : base(settings, logger)
+        {
+            Exchange = settings.ParticipationExchange;
+            RoutingKey = settings.CleanExecutionRoutingKey;
+        }
+
+    }
+}
